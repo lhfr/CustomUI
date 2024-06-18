f8301d65e1ee0306d1dec40654d16c453264f63b
white666 <8967008+whites666@user.noreply.gitee.com>
2024-06-18 17:05:17 +0800

    update dist

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   // outputPath: 'docs-dist',
+  base: '/CustomUI/',
+  publicPath: '/CustomUI/',
   favicons: [
     'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
   ],
